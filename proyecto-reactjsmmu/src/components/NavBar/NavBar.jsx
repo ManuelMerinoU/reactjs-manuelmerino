d3ace55300eef4666d4cdcd1573573a97bb22e35
ManuelMU <manumerino01@gmail.com>
2022-04-17 13:45:57 -0300

    Nav

---
+++ proyecto-reactjsmmu/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './NavBar.css'
+
+function NavBar(props) {
+    return (
+        <div className='nav'>
+            <div id='titulo'>  
+                <h1> <a href="index.html"> About Saints Studio</a> </h1>
+            </div>
+            <ul>
+                <li className='active'> <a href="index.html"> Inicio </a> </li>
+
+                <li> <a href="#Nosotros"> sobre nosotros </a> </li>
+
+                <li> <a href="Usuario.html"> usuario </a> </li>
+
+                <li> <a href="Productos.html"> Productos </a> </li>
+
+                <li> <a href="Contactanos.html"> Contactanos </a> </li>
+
+                <li> <a href="SoporteT.html"> Soporte tecnico </a> </li>
+
+                <li> <a href="#footer"> Derechos de Autor </a> </li>
+            </ul>
+        </div>
+    );
+}
+
+export default NavBar;
